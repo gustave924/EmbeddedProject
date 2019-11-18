8bb2b21674c10f746dc8e2b8168ecc1711b784e2
gustave924 <madstorm20@gmail.com>
2019-11-18 16:35:27 +0200

    NewCommit

--- AngleOMeter.py
+++ AngleOMeter.py
@@ -1,32 +1,25 @@
 import time
+import RPi.GPIO as GPIO 
 from AngleMeterAlpha import AngleMeterAlpha
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-servo = 11
-GPIO.setup(servo, GPIO.OUT)
-def Drive_servo(roll,pitch):
-        
-        if abs(pitch) < 60 and abs(pitch) != 0:
-                pwm = GPIO.PWM (servo,50)
-                pwm.start(7)
-                DC = (abs((roll)*1.5)/18)+2
-                pwm.ChangeDutyCycle(DC)
-              
-        else:
-                pwm = GPIO.PWM (servo,50)
-                pwm.start(7)
-                pwm.stop() 
-                
-        time.sleep(0.05)
 
-        
+GPIO.setmode(GPIO.BOARD)
+servopin=11
+GPIO.setup(servopin,GPIO.OUT)
+pwm=GPIO.PWM(servopin,50)
 
 angleMeter = AngleMeterAlpha()
 angleMeter.measure()
-while True:
-        
-        print(angleMeter.get_kalman_roll()+19,",,",angleMeter.get_kalman_pitch()-6,".")
-        Drive_servo(angleMeter.get_kalman_pitch()-6, angleMeter.get_kalman_roll()+19 )
-        #print(angleMeter.get_int_roll(), angleMeter.get_int_pitch())
-        time.sleep(0.3)
 
+while True:
+    roll=angleMeter.get_kalman_roll()
+    pitch=angleMeter.get_int_pitch()
+    #print(angleMeter.get_kalman_roll(),",", angleMeter.get_complementary_roll(), ",",angleMeter.get_kalman_pitch(),",", angleMeter.get_complementary_pitch(),".")
+    if (abs(pitch)<=45):
+        pwm.start(0)
+        DC= ((1/18)*abs(roll))+2
+        print(DC,roll,pitch)
+        pwm.ChangeDutyCycle(DC)
+        time.sleep(0.08)
+
+pwm.stop()
+GPIO.cleanup()
